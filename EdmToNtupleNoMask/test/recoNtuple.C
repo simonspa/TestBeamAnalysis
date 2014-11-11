63b9e6f759795f6a31dc85507bd3d7277ce196ce
Simon Spannagel <simon.spannagel@desy.de>
2014-11-11 17:01:53 +0100

    More changes and cleanup

--- EdmToNtupleNoMask/test/recoNtuple.C
+++ EdmToNtupleNoMask/test/recoNtuple.C
@@ -34,29 +34,39 @@ void recoNtuple(){
   duttree->SetBranchAddress("dut1_row", &strip_S1);
   duttree->SetBranchAddress("dut2_row", &strip_S2);
   duttree->SetBranchAddress("dut3_row", &strip_S3);
-  duttree->SetBranchAddress("dut0_channel", &channel_S0);
-  duttree->SetBranchAddress("dut1_channel", &channel_S1);
-  duttree->SetBranchAddress("dut2_channel", &channel_S2);
-  duttree->SetBranchAddress("dut3_channel", &channel_S3);
   duttree->SetBranchAddress("tdcPhase", &tdcPhase);
-	
-  int dutEntries = duttree->GetEntries();
 
+
+  // Book all histograms:
   TH1F *h_row_S0 = new TH1F("row_S0","row_S0", 256, 0. , 256.);
   TH1F *h_row_S1 = new TH1F("row_S1","row_S1", 256, 0. , 256.);
   TH1F *h_row_S2 = new TH1F("row_S2","row_S2", 256, 0. , 256.);
   TH1F *h_row_S3 = new TH1F("row_S3","row_S3", 256, 0. , 256.);
+
+  TH1F *h_row_S0_masked = new TH1F("row_S0_masked","row_S0_masked", 256, 0. , 256.);
+  TH1F *h_row_S1_masked = new TH1F("row_S1_masked","row_S1_masked", 256, 0. , 256.);
   TH1F *h_row_S2_masked = new TH1F("row_S2_masked","row_S2_masked", 256, 0. , 256.);
   TH1F *h_row_S3_masked = new TH1F("row_S3_masked","row_S3_masked", 256, 0. , 256.);
 
   TH1F *h_clusters_S2 = new TH1F("clusters_S2","clusters_S2", 256, 0. , 256.);
   TH1F *h_clusters_S3 = new TH1F("clusters_S3","clusters_S3", 256, 0. , 256.);
 
-  //creat recofile
+
+  // Prepare output Reco ROOT Tree:
+  std::vector<int> clusterwidth_S0;
+  std::vector<double> clusterposition_S0;
+  std::vector<int> clusterwidth_S1;
+  std::vector<double> clusterposition_S1;
+  std::vector<int> clusterwidth_S2;
+  std::vector<double> clusterposition_S2;
+  std::vector<int> clusterwidth_S3;
+  std::vector<double> clusterposition_S3;
   std::vector<double> stubs_M1;
   std::vector<double> stubs_M2;
-  std::vector<int> *row_S2_masked = 0;
-  std::vector<int> *row_S3_masked = 0;
+  std::vector<int> strips_masked_S0;
+  std::vector<int> strips_masked_S1;
+  std::vector<int> strips_masked_S2;
+  std::vector<int> strips_masked_S3;
 
   TFile *recofile = new TFile("RECO_USC.00000478.0001.A.storageManager.00.0000.root","RECREATE");
   TTree *recotree = new TTree("recotree","reconstructed");
@@ -66,26 +76,26 @@ void recoNtuple(){
   recotree->Branch("row_S1", &strip_S1);
   recotree->Branch("row_S2", &strip_S2);
   recotree->Branch("row_S3", &strip_S3);
-  recotree->Branch("row_S2_masked", &row_S2_masked);
-  recotree->Branch("row_S3_masked", &row_S3_masked);
-  recotree->Branch("channel_S0", &channel_S0);
-  recotree->Branch("channel_S1", &channel_S1);
-  recotree->Branch("channel_S2", &channel_S2);
-  recotree->Branch("channel_S3", &channel_S3);
-  recotree->Branch("clusters_S0", &clusters_position_S0);
-  recotree->Branch("clusters_S1", &clusters_position_S1);
-  recotree->Branch("clusters_S2", &clusters_position_S2);
-  recotree->Branch("clusters_S3", &clusters_position_S3);
+  recotree->Branch("row_S0_masked", &strips_masked_S0);
+  recotree->Branch("row_S1_masked", &strips_masked_S1);
+  recotree->Branch("row_S2_masked", &strips_masked_S2);
+  recotree->Branch("row_S3_masked", &strips_masked_S3);
+  recotree->Branch("clusters_S0", &clusterposition_S0);
+  recotree->Branch("clusters_S1", &clusterposition_S1);
+  recotree->Branch("clusters_S2", &clusterposition_S2);
+  recotree->Branch("clusters_S3", &clusterposition_S3);
   recotree->Branch("clusterwidth_S0", &clusterwidth_S0);
   recotree->Branch("clusterwidth_S1", &clusterwidth_S1);
   recotree->Branch("clusterwidth_S2", &clusterwidth_S2);
   recotree->Branch("clusterwidth_S3", &clusterwidth_S3);
-  recotree->Branch("stubs_module_1",&stubs_module_1);
-  recotree->Branch("stubs_module_2",&stubs_module_2);
+  recotree->Branch("stubs_module_1",&stubs_M1);
+  recotree->Branch("stubs_module_2",&stubs_M2);
   recotree->Branch("tdc", &tdcPhase);
 
-  //events loop
-  for (int i = 0; i < dutEntries; i++) {
+
+
+  // The main events loop:
+  for (int i = 0; i < duttree->GetEntries(); i++) {
 
     duttree->GetEntry(i);
 
@@ -93,12 +103,10 @@ void recoNtuple(){
     // If a strip is "noisy" can be read from the hit map histograms
     // and results in a hardcoded cut on strip numbers.
 
-    // Store all strips which are fine in vectors for further analyses:
-    std::vector<int> S0_strips_masked;
-    std::vector<int> S1_strips_masked;
-    std::vector<int> S2_strips_masked;
-    std::vector<int> S3_strips_masked;
-	
+    // Store all strips which are fine in vectors for further analyses
+    // strips_masked_S0;
+
+
     // Clustering
     // On each sensor, cluster all strips which belong together. 
     // The most simple solution is to just add all up which are next to each other
@@ -121,49 +129,31 @@ void recoNtuple(){
       // ...
     }
 
+    // Some extra cross check that we didn't mess things up:
     if(cluster_firstStrip_S0.size() != cluster_lastStrip_S0.size()){
       std::cerr << "Error in vector size cluster/S0 for first/last strip.\n";
-      exit(583);
+      exit;
     }
 
     //Calculate the cluster width and position for all sensors:
-    std::vector<int> clusterwidth_S0;
-    std::vector<double> clusterposition_S0;
-
-    std::vector<int> clusterwidth_S1;
-    std::vector<double> clusterposition_S1;
-
-    std::vector<int> clusterwidth_S2;
-    std::vector<double> clusterposition_S2;
-
-    std::vector<int> clusterwidth_S3;
-    std::vector<double> clusterposition_S3;
-
     for(size_t strip = 0; strip < cluster_firstStrip_S0.size(); ++strip) {
       // ...
+      // clusterwidth_S0
+      // clusterposition_S0
     }
+    // ...
 
-		
+
     // Produce the stubs for the two modules
-    // Stubs are defined between sensros S0 and S1 (module 1) and S2/S3 (module2).
-
+    // Stubs are defined between sensors S0 and S1 (module 1) and S2/S3 (module 2).
 		
-    //stubs module 1
-    if ( v_firstStripOfCluster.size() == 1 &&  v_firstStripOfCluster1.size() == 1){
-      for(size_t iStrip = 0; iStrip < v_firstStripOfCluster.size() && 	v_firstStripOfCluster1.size(); ++iStrip){
-	double pos_S0 = (double(v_firstStripOfCluster.at(0) + v_lastStripOfCluster.at(0))/2.);
-	double pos_S1 = (double(v_firstStripOfCluster1.at(0) + v_lastStripOfCluster1.at(0))/2.);
-	stubs_module_1.push_back(pos_S1 - pos_S0);
-      }
+    // Stubs Module 1:
+    // (restricting to one-cluster events)
+    if(clusterposition_S0.size() == 1 && clusterposition_S1.size() == 1) {
+      stubs_M1.push_back(clusterposition_S1.front() - clusterposition_S0.front());
     }
-    //stubs module 2
-    if ( v_firstStripOfCluster2.size() == 1 && v_firstStripOfCluster3.size() == 1){
-      for(size_t iStrip = 0; iStrip < v_firstStripOfCluster2.size() && v_firstStripOfCluster3.size(); ++iStrip){
-	double pos_S2 = (double(v_firstStripOfCluster2.at(0) + v_lastStripOfCluster2.at(0))/2.);
-	double pos_S3 = (double(v_firstStripOfCluster3.at(0) + v_lastStripOfCluster3.at(0))/2.);
-	stubs_module_2.push_back(pos_S3 - pos_S2);
-      }
-    }
+    // ...
+
 
     recotree->Fill();
 
@@ -180,10 +170,11 @@ void recoNtuple(){
     stubs_M1.clear();
     stubs_M2.clear();
 
-    row_S2_masked->clear();
-    row_S3_masked->clear();
+    strips_masked_S0->clear();
+    strips_masked_S1->clear();
+    strips_masked_S2->clear();
+    strips_masked_S3->clear();
 
-	
   } //end of events loop
 
 
