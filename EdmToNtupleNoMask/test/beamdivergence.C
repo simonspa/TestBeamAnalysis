39ac8283951b391cc8248258230afd10d9b25c05
Simon Spannagel <simon.spannagel@desy.de>
2014-11-17 11:23:47 +0100

    Rework beamdivergence exercise.

--- EdmToNtupleNoMask/test/beamdivergence.C
+++ EdmToNtupleNoMask/test/beamdivergence.C
@@ -11,45 +11,51 @@
 #include "TStyle.h"
 #include "stdio.h"
 
+void beamdivergence(){
 
-void BeamDiv(){
+  // Book histogram:
+  TH1F *h_div = new TH1F("","; #mum; Events", 100, -2000. , 2000.);
 
-	std::vector<double> *clusters_position_S0= 0;
-	std::vector<double> *clusters_position_S1= 0;
- 	std::vector<double> *clusters_position_S2= 0;
-	std::vector<double> *clusters_position_S3= 0;
+  // Prepare input data:
+  std::vector<double> *clusters_position_S0= 0;
+  std::vector<double> *clusters_position_S1= 0;
+  std::vector<double> *clusters_position_S2= 0;
+  std::vector<double> *clusters_position_S3= 0;
 
-	TH1F *h_div = new TH1F("","; #mum; Events", 100, -2000. , 2000.);
+  // Use a zero-degree run for this analysis:
+  TFile *recofile =  TFile::Open("MyRecoTTreeFile.root");
+  TTree *recotree = (TTree *) recofile->Get("recotree");
+  recotree->SetBranchAddress("clusters_S0", &clusters_position_S0);
+  recotree->SetBranchAddress("clusters_S1", &clusters_position_S1);
+  recotree->SetBranchAddress("clusters_S2", &clusters_position_S2);
+  recotree->SetBranchAddress("clusters_S3", &clusters_position_S3);
 
+  int dutEntries = recotree->GetEntries();
 
-	TFile *recofile =  TFile::Open("RECO_USC.00000478.0001.A.storageManager.00.0000.root");
-	TTree *recotree = (TTree *) recofile->Get("recotree");
-	recotree->SetBranchAddress("clusters_S0", &clusters_position_S0);
-	recotree->SetBranchAddress("clusters_S1", &clusters_position_S1);
-    	recotree->SetBranchAddress("clusters_S2", &clusters_position_S2);
-	recotree->SetBranchAddress("clusters_S3", &clusters_position_S3);
+  // Run over all events in the tree:
+  for (int i =0; i < dutEntries; i++){
+    recotree->GetEntry(i);
 
-	int dutEntries = recotree->GetEntries();
+    unsigned int cluster_size_S0 = clusters_position_S0->size();
+    unsigned int cluster_size_S1 = clusters_position_S1->size();
+    unsigned int cluster_size_S2 = clusters_position_S2->size();
+    unsigned int cluster_size_S3 = clusters_position_S3->size();
 
-	for (int i =0; i < dutEntries; i++){
-		recotree->GetEntry(i);
+    // Limit to event with only one cluster per sensor
+    if(cluster_size_S0 == 1 && cluster_size_S1 == 1 && cluster_size_S2 == 1 && cluster_size_S3 == 1) {
 
-		unsigned int   cluster_size_S0 = clusters_position_S0->size();
-		unsigned int   cluster_size_S1 = clusters_position_S1->size();
-		unsigned int   cluster_size_S2 = clusters_position_S2->size();
-		unsigned int   cluster_size_S3 = clusters_position_S3->size();
+      // By applying a cut to the stubs we get a cleaner signal:
+      if(TMath::Abs(clusters_position_S0->front()-clusters_position_S1->front()) < 4. && TMath::Abs(clusters_position_S2->front()-clusters_position_S3->front()) < 4.) {
+	// Fill histogram:
+	// Average cluster positions on both M1 and M2
+	// Subtract averaged cluster position M2 from M1
+	// What do we have to do about the different pitches of the Modules?
+	h_div->Fill(1);
+      }
+    }
+  }
 
-		if (  cluster_size_S0 == 1 &&  cluster_size_S1 == 1 && cluster_size_S2 == 1 &&  cluster_size_S3 == 1){
-			for(size_t iStrip = 0; iStrip < cluster_size_S0; ++iStrip){
-				
-	if(TMath::Abs(clusters_position_S0->at(0) - clusters_position_S1->at(0)) < 4. && TMath::Abs(clusters_position_S2->at(0) - clusters_position_S3->at(0)) < 4.){
-					h_div->Fill((((clusters_position_S0->at(0)+clusters_position_S1->at(0))/2.)*90.) - (((clusters_position_S2->at(0)+clusters_position_S3->at(0))/2.)*80.) - 6.01015e+02);
-				}
-			}
-		}
-	}
-
-	gStyle->SetOptFit(1011);
-	h_div->Draw();
+  gStyle->SetOptFit(1011);
+  h_div->Draw();
 
 }
